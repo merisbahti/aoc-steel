f9c5ea84dbafd37c684e2bc5bb1cd6dd74a77536
meris <meris.bahti@gmail.com>
2024-12-01 07:33:48 +0100

    save test

---
+++ test.scm
@@ -0,0 +1,33 @@
+(define *failed-assertions* (vector))
+(define-syntax assert
+  (syntax-rules ()
+    [(assert a b)
+      (let* (
+             [aEvaled a]
+             [bEvaled b]
+             [aStringified (trim-start-matches (to-string 'a) "'")]
+             [bStringified (trim-start-matches (to-string 'b) "'")])
+        (when
+          (not (eq? aEvaled bEvaled))
+          (error-with-span
+            (list
+              (first (#%syntax-span a))
+              (second (#%syntax-span b))
+              (third (#%syntax-span a)))
+            (string-join
+              (list
+                (to-string)
+                aStringified
+                " is not equal to "
+                bStringified
+                " ("
+                (to-string aEvaled)
+                " !== "
+                (to-string bEvaled)
+                ")")))
+          (displayln)))]))
+
+(define (run-tests)
+  (for-each (lambda (x) (displayln x)) (vector->list *failed-assertions*)))
+
+;; assert(1+2, 3+4)
