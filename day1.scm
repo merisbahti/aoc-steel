4b2c53399157494d8bb7a8c3365a932c945eac32
meris <meris.bahti@gmail.com>
2024-12-01 08:33:09 +0100

    sol2 solved

--- day1.scm
+++ day1.scm
@@ -13,6 +13,7 @@
     ((null? into) (list thing))
     ((< thing (car into)) (cons thing into))
     (else (cons (first into) (insert-sort thing (rest into))))))
+
 (define (reduce-list fst-list snd-list acc)
   (cond [
          (and
@@ -31,7 +32,6 @@
     (reduce
       (fn (curr acc)
         (define split (split-once curr "   "))
-        (displayln curr split)
         (define snd-nr (string->number (second split)))
         (define fst-nr (string->number (first split)))
         (list
@@ -44,3 +44,36 @@
 (assert (sol1 test-input) 11)
 
 (assert (sol1 real-input) 1882714)
+
+(define (sol2 input)
+  (define lines (split-many (trim input) "\n"))
+  (define lists
+    (reduce
+      (fn (curr acc)
+        (define split (split-once curr "   "))
+        (displayln curr split)
+        (define snd-nr (string->number (second split)))
+        (define fst-nr (string->number (first split)))
+        (list
+          (insert-sort fst-nr (first acc))
+          (insert-sort snd-nr (second acc))))
+      (list '() '())
+      lines))
+  (define occurences-hash
+    (reduce
+      (fn (curr acc)
+        (define curr-value (if (hash-contains? acc curr) (hash-ref acc curr) 0))
+        (hash-insert acc curr (+ 1 curr-value)))
+      (hash)
+      (second lists)))
+
+  (reduce (fn (curr acc)
+           (define occurences
+             (if (hash-contains? occurences-hash curr) (hash-ref occurences-hash curr) 0))
+           (+ acc (* occurences curr)))
+    0
+    (first lists)))
+
+(assert (sol2 test-input) 31)
+
+(assert (sol2 real-input) 1882714)
