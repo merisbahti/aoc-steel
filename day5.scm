9f315f4bfcdd27b89baee3086aa47b337262e89c
meris <meris.bahti@gmail.com>
2024-12-09 10:51:29 +0100

    day5 init

--- day5.scm
+++ day5.scm
@@ -29,15 +29,43 @@
 75,97,47,61,53
 61,13,29
 97,13,75,29,47")
+(define (index-of list elem)
+  (define (index-of-iter list elem curr-index)
+    (cond
+      [(null? list) (None)]
+      [(equal? (first list) elem) curr-index]
+      [else (index-of-iter (cdr list) elem (+ curr-index 1))]))
+  (index-of-iter list elem 0))
+(assert (index-of (list 'a 'b 'c) 'a) 0)
+(assert (index-of (list) 'a) (None))
+(assert (index-of (list 'a 'b 'c) 'c) 2)
+(assert (index-of (list 'a 'b 'c) 'd) (None))
+(define
+  (all? pred list)
+  (cond
+    [(null? list) #t]
+    [else (and (pred (car list)) (all? pred (cdr list)))]))
 
 (define (sol1 input)
   (define full-split (split-once input "\n\n"))
-  (displayln full-split)
-  (define rules (map (fn (row) (split-once row "|")) (~> full-split (first) (split-many "\n"))))
-  (define updates (map (fn (row) (split-many row ",")) (~> full-split (second) (split-many "\n"))))
+  (define rules (map (fn (row) (split-once row "|")) (split-many (first full-split) "\n")))
+  (define updates (map (fn (row) (split-many row ",")) (split-many (second full-split) "\n")))
 
   (displayln rules)
-  (displayln updates)
+  (for-each displayln updates)
+  (define ok-updates
 
-  0)
+    (filter
+      (fn (update)
+        (all?
+          (fn (rule)
+            (define fst-index (index-of update (first rule)))
+            (define snd-index (index-of update (second rule)))
+            (when (or (None? fst-index) (None? snd-index)) (return! #t))
+            (define res (< fst-index snd-index))
+            res)
+          rules))
+      updates))
+  (sum (map string->number (map (fn (update) (list-ref update (floor (/ (length update) 2)))) ok-updates))))
+
 (assert (sol1 test-input) 143)
